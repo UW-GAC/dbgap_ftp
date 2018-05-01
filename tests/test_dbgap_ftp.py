236b14f382cfc144abf68c891585f4cae25f6133
Adrienne Stilp <amstilp@uw.edu>
2018-05-01 13:22:03 -0700

    Change name of object created in tests
    Instead of calling the DbgapFtp object "cnx", it is clearer to just
    call it "object".

--- tests/test_dbgap_ftp.py
+++ tests/test_dbgap_ftp.py
@@ -13,11 +13,11 @@ KNOWN_PHS = 16
 class DbgapFtpTest(TestCase):
 
     def setUp(self):
-        self.cnx = dbgap_ftp.DbgapFtp()
+        self.object = dbgap_ftp.DbgapFtp()
 
     def test_init_creates_ftp_connection(self):
         """Creates an ftp connection and stores it as an attribute."""
-        self.assertIsInstance(self.cnx.ftp, FTP)
+        self.assertIsInstance(self.object.ftp, FTP)
 
     def test_init_fails_with_nonexistent_server(self):
         """Fails with proper error if non-existent server is requested."""
@@ -25,12 +25,12 @@ class DbgapFtpTest(TestCase):
             dbgap_ftp.DbgapFtp(server='foo')
 
     def test_get_base_study_directory_works_with_positive_integer(self):
-        self.assertIsInstance(self.cnx._get_base_study_directory(KNOWN_PHS), str)
+        self.assertIsInstance(self.object._get_base_study_directory(KNOWN_PHS), str)
 
     def test_get_base_study_directory_fails_with_negative_number(self):
         with self.assertRaisesRegex(ValueError, dbgap_ftp.DbgapFtp.ERROR_STUDY_VALUE):
-            self.cnx._get_base_study_directory(-1)
+            self.object._get_base_study_directory(-1)
 
     def test_get_base_study_directory_fails_with_zero(self):
         with self.assertRaisesRegex(ValueError, dbgap_ftp.DbgapFtp.ERROR_STUDY_VALUE):
-            self.cnx._get_base_study_directory(0)
+            self.object._get_base_study_directory(0)
